176d51fd19d788c92b9c92340714dc1918867eeb
Michael Ballantyne <ballantyne.m@northeastern.edu>
2023-12-03 21:59:53 -0500

    Factor out test-student-executable

--- main.rkt
+++ main.rkt
@@ -189,33 +189,45 @@
   (define staff-tests (hash-ref cfg staff-tests-path))
   (define assn-name-str (~a (hash-ref cfg assignment-name)))
   (define commits-data (file->value (hash-ref cfg team-commits)))
-  (for ([this-name-str (hash-keys commits-data)])
-    (define team-r-dir (build-path results-dir this-name-str))
-    (void (ensure-dir team-r-dir))
-    (run-make! (build-path s-root this-name-str assn-name-str) team-r-dir)
+  (for ([team (hash-keys commits-data)])
+    (define team-r-dir (build-path results-dir team))
+    (ensure-dir team-r-dir)
+    (run-make! (build-path s-root team assn-name-str) team-r-dir)
+    (define team-exe (build-path s-root team s-exe-name))
     (define team-mf (build-path team-r-dir MF.txt))
-    (unless (file-exists? team-mf)
-      (define (write-team-output str)
-        (with-output-to-file team-mf (lambda () (displayln str))))
-      (define team-exe (build-path s-root this-name-str s-exe-name))
-      (log-cs4500-f18-warning "about to test student executable '~a', current ps -f:~n~a" (path->string team-exe) (current-process-list))
-      (cond
-        [(not (file-exists? team-exe))
-         (write-team-output (format "file '~a' does not exist" (path->string team-exe)))]
-        [(not (file-executable? team-exe))
-         (write-team-output (format "file '~a' is not executable" (path->string team-exe)))]
-        [else
-         (log-cs4500-f18-warning "executing ~a" (path->string team-exe))
-         (define r-str
-           (parameterize ((current-directory (path-only team-exe)))
-              (run-staff-harness cfg #:exe team-exe #:tests staff-tests)))
-         (write-team-output r-str)
-         (log-cs4500-f18-warning "finished executing '~a', current ps -f:~n~a~noutput:~n~a"
-                                 (path->string team-exe)
-                                 (current-process-list)
-                                 r-str)
-         (void)])))
+    (test-student-executable 
+      team-exe
+      (lambda ()
+        (run-staff-harness cfg #:exe team-exe #:tests staff-tests))
+      team-mf)
+
+    )
   (void))
+
+;; If the student executable `exe-path` exists, run it by calling `run-thunk` in the executable's directory.
+;; Either way, record the output of the run or failure diagnostics to the `out-file`.
+(define (test-student-executable exe-path run-thunk out-file)
+  (define (write-team-output str)
+    (with-output-to-file out-file (lambda () (displayln str))))
+
+  (unless (file-exists? out-file)
+    (log-cs4500-f18-warning "about to test student executable '~a', current ps -f:~n~a" (path->string exe-path) (current-process-list))
+    (cond
+      [(not (file-exists? exe-path))
+       (write-team-output (format "file '~a' does not exist" (path->string exe-path)))]
+      [(not (file-executable? exe-path))
+       (write-team-output (format "file '~a' is not executable" (path->string exe-path)))]
+      [else
+       (log-cs4500-f18-warning "executing ~a" (path->string exe-path))
+       (define r-str
+         (parameterize ((current-directory (path-only exe-path)))
+            (run-thunk)))
+       (write-team-output r-str)
+       (log-cs4500-f18-warning "finished executing '~a', current ps -f:~n~a~noutput:~n~a"
+                               (path->string exe-path)
+                               (current-process-list)
+                               r-str)
+       (void)])))
 
 (define (staff-exe-vs-student-test results-dir cfg)
   ;; for every team name,
@@ -231,12 +243,12 @@
   (define passing 0)
   (define failing 0)
   (define commits-data (file->value (hash-ref cfg team-commits)))
-  (for ((this-name-str (hash-keys commits-data)))
-    (define this-r (build-path results-dir this-name-str))
+  (for ((team (hash-keys commits-data)))
+    (define this-r (build-path results-dir team))
     (define this-r-test (build-path this-r test-name))
     (unless (directory-exists? this-r-test)
       (make-directory this-r-test)
-      (define this-tests (build-path s-root this-name-str test-path))
+      (define this-tests (build-path s-root team test-path))
       (define expected-files (for*/set ([i (in-range num-tests)]
                                         [in? (in-list '(#t #f))])
                                (if in? (i-in.json i) (i-out.json i))))
@@ -245,7 +257,7 @@
           (for/set ([p (in-list (directory-list this-tests))]) (path->string p))
           (set)))
       (unless (equal? expected-files actual-in-dir)
-        (log-cs4500-f18-info "Extra/Missing test files for ~a" this-name-str)
+        (log-cs4500-f18-info "Extra/Missing test files for ~a" team)
         (with-output-to-file (build-path this-r AUDIT.txt) #:exists 'append
           (lambda () (printf "Extra or missing files in tests directory\n"))))
       (for* ([i (in-range num-tests)]
@@ -308,21 +320,21 @@
   (define s-root (hash-ref cfg student-root))
   (define s-path (hash-ref cfg student-exe-name))
   (define commits-data (file->value (hash-ref cfg team-commits)))
-  (for ((this-name-str (hash-keys commits-data))
-        #:when (and (has-valid-testfest-exe? this-name-str results-dir cfg)
-                    (not (skip-fest? this-name-str results-dir cfg))))
-    (log-cs4500-f18-info "testfest '~a' vs ..." this-name-str)
+  (for ((team (hash-keys commits-data))
+        #:when (and (has-valid-testfest-exe? team results-dir cfg)
+                    (not (skip-fest? team results-dir cfg))))
+    (log-cs4500-f18-info "testfest '~a' vs ..." team)
     (log-cs4500-f18-warning "Current ps -f:~n~a" (current-process-list))
-    (define this-exe (build-path s-root this-name-str s-path))
-    (define student-r-dir (build-path results-dir this-name-str))
+    (define this-exe (build-path s-root team s-path))
+    (define student-r-dir (build-path results-dir team))
     (for ((that-name-str (hash-keys commits-data))
-          #:when (and (not (equal? this-name-str that-name-str))
+          #:when (and (not (equal? team that-name-str))
                       (let ((that-tests (make-tests-dir that-name-str results-dir cfg)))
                         (non-empty-directory-exists? that-tests))))
       (define that.txt (build-path student-r-dir (format "~a.txt" that-name-str)))
       (define that-tests (make-tests-dir that-name-str results-dir cfg))
       (unless (file-exists? that.txt)
-        (log-cs4500-f18-info "testfest '~a' vs '~a'" this-name-str that-name-str)
+        (log-cs4500-f18-info "testfest '~a' vs '~a'" team that-name-str)
         (log-cs4500-f18-info "running xtest ~s ~s" (path-string->string this-exe) (path-string->string that-tests))
         (parameterize ((current-directory (path-only this-exe)))
           (with-output-to-file that.txt
